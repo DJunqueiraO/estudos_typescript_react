1c887667475ea366ee099819e01ad411ec0dc7ee
FOURSYS\djunqueira <Batata>
2024-07-05 14:36:57 -0300

    testes

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,24 +5,22 @@ export function Home(props: React.HTMLAttributes<HTMLDivElement>) {
   return (
     <div className='home'>
       <Grid align_columns='true'>
-        {
-          [
+        {[
             [1, 2, 3],
             [1, 2],
-            [1, 2, 3, 4, 5],
-          ]
-        }
+            [1, 2, 3, 4, 5]
+        ]}
       </Grid>
       <Table
         thead_children={[
           [1, 2, 3],
           [1, 2],
-          [1, 2, 3, 4, 5],
+          [1, 2, 3, 4, 5]
         ]}
         tbody_children={[
           [1, 2, 3],
           [1, 2],
-          [1, 2, 3, 4, 5],
+          [1, 2, 3, 4, 5]
         ]}
         />
     </div>
